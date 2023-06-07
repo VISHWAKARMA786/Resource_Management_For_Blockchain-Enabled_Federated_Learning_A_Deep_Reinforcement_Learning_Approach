ef3e500793566cd533add4816e62b2bc2d5f5f54
jamvant vishwakarma <51391760+VISHWAKARMA786@users.noreply.github.com>
2023-06-07 13:30:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,89 @@
+import numpy as np
+import pandas as pd
 from central_server import CentralServer
 from client import Client
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 MAX_EPISODE_LENGTH = 10                   # define the max episode length
 TASK_COMPLETION_STATE = "order_complete"  # define the task completition state
 TIME_LIMIT = 60                           # define the time limit per episode (in seconds)
 TERMINATION_COMMAND = "terminate"         # define user defined termination command
 
+# Step 1: Preprocess Text Data (Example: Tokenization)
+def preprocess_text(text):
+    # Perform tokenization
+    tokens = text.split()
+    return tokens
+
+# step 3: Define State Shape
+state_shape =  (max_sequence_length, embedding_dim) # Examples values
+
+# Step 4: Define Action Space
+action_space = num_actions  # Example value
+
+# step 5: panda import value with datasets
+
+# Step 6: Create Training, Validation, and Test Sets
+
+train_data, test_data = train_test_split(news_data, test_size=0.2, random_state=42)
+train_data, val_data = train_test_split(train_data, test_size=0.2, random_state=42)
+
+# Step 2: Convert Text to Numeric Representation (Example: TF-IDF)
+vectorizer = TfidfVectorizer(tokenizer=preprocess_text)
+train_features = vectorizer.fit_transform(train_data)
+val_features = vectorizer.transform(val_data)
+test_features = vectorizer.transform(test_data)
+
+# Step 7: Modify the Step Function
+def step(state, action):
+    # Perform an environment step based on the chosen action
+    next_state =  # Update the state based on the chosen action
+    reward =  # Calculate the reward based on the transition
+    done =  # Check if the episode is done
+
+    return next_state, reward, done
+
+# Step 8: Modify the Reward Function
+def reward(state, action, next_state):
+    # Calculate the reward based on the current state, chosen action, and next state
+    reward =  # Calculate the reward value
+
+    return reward
+
+# Step 9: Modify the Done Function
+def done(state):
+    # Check if the episode is done based on the current state
+    done =  # Determine whether the episode is done based on the state
+
+    return done
+
+# Step 10: Update the main() function with the new code
+
 
 def main():
-    state_shape =  (max_sequence_length,)
-    action_space =  num-action
-    num_iterations =  
+    num_iterations =  100
+
 
     # Create an instance of the central server
     central_server = CentralServer(state_shape, action_space)
     central_server.start()
+
+    # Distribute initial model to clients
+    for client in central_server.clients:
+        client.set_model(central_server.model)
+
+        # Training loop
+    for iteration in range(num_iterations):
+        central_server.aggregate_model_updates()
+         
+         # Evaluation
+    central_server.evaluate_model(test_features)
+
+    best_validation_loss = float('inf')
+    consecutive_no_improvement = 0
+
 
     # Run the iterative training process
     for iteration in range(num_iterations):
@@ -28,25 +97,53 @@ def main():
         # Step 7: Communication and Aggregation
         central_server.aggregate_model_updates()
 
-    # Evaluate the final global model
-    evaluate_model(central_server.model)
+        validation_loss = evaluate_model(central_server.model, val_features)
+        if validation_loss < best_validation_loss:
+            best_validation_loss = validation_loss
+            consecutive_no_improvement = 0
+        else:
+            consecutive_no_improvement += 1
+            if
+            consecutive_no_improvement >= 5:
+            print("Convergence Reached. Stopping Training.")
+            break
 
-def evaluate_model(global_model):
-    num_episodes =  # Define the number of evaluation episodes
+
+    # Evaluate the final global model
+    evaluate_model(central_server.model, test_features)
+    
+
+def evaluate_model(global_model, dataset):
+    num_episodes = 5  # Define the number of evaluation episodes
     total_reward = 0
 
     # Perform evaluation on the test environment
     for _ in range(num_episodes):
         state =  # Initialize the environment state
 
-        while not is_done(state):  # Define the termination condition
+        episode_length = 0
+        start_time = time.time()
+
+        while not is_done(state, episode_length, start_time):  # Define the termination condition
             q_values = global_model(tf.expand_dims(state, axis=0))
             action = tf.argmax(q_values, axis=1)[0].numpy()
-            state, reward, done =  # Perform environment step
+            state, reward, done = step(state, action) # Perform environment step
             total_reward += reward
+            episode_length  += 1
 
     average_reward = total_reward / num_episodes
     print("Average reward:", average_reward)
+    def is_done(state, episode_length, start_time):
+        if episode_length >= MAX_EPISODE_LENGTH:
+            return True
+        if state == TASK_COMPLETION_STATE:
+            return True
+        current_time = time.time()
+        if current_time - start_time >= TIME_LIMIT:
+            return True
+        if state.lower() ==TERMINATION_COMMAND:
+            return True
+        return False
 
 if __name__ == "__main__":
     main()
