bef2ac68204782e22587fd00af2c06221729c28a
jamvant vishwakarma <51391760+VISHWAKARMA786@users.noreply.github.com>
2023-06-09 14:00:22 +0530

    Update central_server.py

--- central_server.py
+++ central_server.py
@@ -1,29 +1,28 @@
 import socket
-from network import DQN
 from client import Client
-
-# Define the central server class
-
 
 class CentralServer:
     def __init__(self, state_shape, action_space):
         self.clients = []
-        self.model = DQN(state_shape, action_space)
+        self.model = None
+        self.state_shape = state_shape
+        self.action_space = action_space
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address = ('localhost', 8888)
-        self.server_socket.bind(self.server_address)
 
     def start(self):
+        self.server_socket.bind(self.server_address)
         self.server_socket.listen(5)
         print("Central server started. Waiting for connections...")
 
         while True:
             client_socket, client_address = self.server_socket.accept()
             print("New client connected:", client_address)
-            client = Client(client_socket)
+            client = Client(client_socket, self.state_shape, self.action_space)
             self.clients.append(client)
 
     def distribute_initial_model(self):
+        self.model = self.create_model()
         initial_model_weights = self.model.get_weights()
         for client in self.clients:
             client.send_model_weights(initial_model_weights)
@@ -37,12 +36,34 @@ class CentralServer:
         aggregated_weights = self.aggregate_weights(updated_weights)
         self.model.set_weights(aggregated_weights)
 
+    def create_model(self):
+        # Define and compile the model
+        model = ...  # Define your model architecture here
+        model.compile(optimizer='adam', loss='mse')
+        return model
+
     def aggregate_weights(self, weights_list):
-        # Code to perform aggregation of weights using federated averaging or other methods
+        # Implement the logic for aggregating model weights
+        # Return the aggregated weights
+        return None
 
-        pass
+    def evaluate_model(self, features):
+        # Implement the logic for evaluating the model on the provided features
+        # Return the evaluation results (e.g., loss or any other metrics of interest)
+        return None
 
-    if __name__ == "__main__":
+    def run_training(self):
+        self.distribute_initial_model()
+
+        for client in self.clients:
+            client.local_training()
+
+        self.aggregate_model_updates()
+        test_loss = self.evaluate_model(test_features)
+        print("Test Loss:", test_loss)
+
+
+if __name__ == "__main__":
     state_shape = ...
     action_space = ...
 
