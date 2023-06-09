589b8a10a932e97b7780af8280945135c5e50bd1
jamvant vishwakarma <51391760+VISHWAKARMA786@users.noreply.github.com>
2023-06-09 13:56:20 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,43 +2,64 @@ import tensorflow as tf
 from network import DQN
 import socket
 
-
-
 class Client:
-    def __init__(self, local_data, socket, state_shape, action_space):
+    def __init__(self, socket, state_shape, action_space):
         self.socket = socket
-        self.local_data = local_data
         self.model = DQN(state_shape, action_space)
 
+    def local_training(self):
+        optimizer = tf.keras.optimizers.Adam()
+        loss_fn = tf.keras.losses.MeanSquaredError()
 
-def local_training(self):
-    optimizer = tf.keras.optimizers.Adam()
-    loss_fn = tf.keras.losses.MeanSquaredError()
+        # Define the number of epochs
+        num_epochs = ...
 
-    #  define the number of epochs
-    num_epochs = ....
-    
-    for epoch in range(num_epochs):
-        with tf.GradientTape() as tape:
-            # Get inputs and targets from the local dataset
-            inputs, targets = self.local_data
+        for epoch in range(num_epochs):
+            with tf.GradientTape() as tape:
+                # Get inputs and targets from the local dataset
+                inputs, targets = self.get_local_data()
 
-            # Forward pass
-            q_values = self.model(inputs)
+                # Forward pass
+                q_values = self.model(inputs)
 
-            # Calculate the loss
-            loss = loss_fn(targets, q_values)
+                # Calculate the loss
+                loss = loss_fn(targets, q_values)
 
-        # Backpropagation and update the model weights
-        gradients = tape.gradient(loss, self.model.trainable_variables)
-        optimizer.apply_gradients(
-            zip(gradients, self.model.trainable_variables))
+            # Backpropagation and update the model weights
+            gradients = tape.gradient(loss, self.model.trainable_variables)
+            optimizer.apply_gradients(
+                zip(gradients, self.model.trainable_variables))
+
+        # Implement the logic to retrieve local data for training
+    def get_local_data(self):
+        inputs = tf.random.normal((batch_size, input_shape))
+        targets = tf.random.normal((batch_size, action_space))
+        return inputs, targets
+
+        # Implement the logic to send model weights to the central server
+    def send_model_weights(self, weights):
+        serialized_weights = tf.keras.models.save_model(weights, "temp_weights.h5")
+        self.socket.sendall(serialized_weights)
+        pass
+
+        # Implement the logic to receive model weights from the central server
+    def receive_model_weights(self):
+        recived_data = self.socket.recv(1024)
+        recived_weights = tf.keras.models.load_model(recived_data)
+        return recived_weights
 
 
-def send_model_weights(self, weights):
-    # Code to send model weights to the central server
-    pass
+if __name__ == "__main__":
+    state_shape = (84, 84, 4)
+    action_space = 4
+    # batch_size = 
+    # input_shape = 
 
-def receive_model_weights(self):
-    # reicive the modal weight from the central server
-    pass
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 8888)
+    client_socket.connect(server_address)
+
+    client = Client(client_socket, state_shape, action_space)
+    client.local_training()
+    client.send_model_weights()
+    client.receive_model_weights()
